706ea33b319ea6cba5ad321b894b0ac20c23b821
ThedonJim <royouma50@gmail.com>
2025-03-20 21:27:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ while name == "":
     print("You did not enter your name")
     name = input("Input your name")
 print(f"Hello {name}")
+food = input("What is your favourite food?(q to quit)")
+while not food == "q"
+ print("pick another food (q to quit)")
+food = input("What is your favourite food?(q to quit)")
+
+print(f"Bye")
 
 
 
