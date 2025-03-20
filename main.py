e35b9adc46e8aff96a9206c4f952831ed2747b32
ThedonJim <royouma50@gmail.com>
2025-03-20 21:10:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World")
+languages = ['python','java','C']
+print(languages[0])
+name = input("Input your name: ")
+while name == "":
+    print("You did not enter your name")
+    name = input("Input your name")
+print(f"Hello {name}")
+
+
+
